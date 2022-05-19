2835f34e47b02a82af8db07888cd764746cff6f1
jinha <kjha7448@gmail.com>
2022-05-19 22:38:22 +0900

    1012,2178,2606,2667

--- Class/src/bj2178.java
+++ Class/src/bj2178.java
@@ -5,51 +5,48 @@ import java.util.StringTokenizer;
 import java.util.Queue;
 import java.util.LinkedList;
 public class bj2178{
-    static int N,M;
     static int[][] maze;
     static boolean[][] checked;
+    static int N,M;
     
     public static void main(String[] args) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(br.readLine());
-        N=Integer.parseInt(st.nextToken());
+        N=Integer.parseInt(st.nextToken()); //(N.M)
         M=Integer.parseInt(st.nextToken());
         
-        maze=new int[N][M];
-        checked=new boolean[N][M];
-        for(int i=0;i<N;i++){
+        maze=new int[N+1][M+1];
+        checked=new boolean[N+1][M+1];
+        for(int i=1;i<=N;i++){
             st=new StringTokenizer(br.readLine());
             String str=st.nextToken();
             for(int j=0;j<str.length();j++){
-                maze[i][j]=str.charAt(j)-'0';
+                maze[i][j+1]=str.charAt(j)-'0';
             }
         }
         BFS();
-        System.out.println(maze[N-1][M-1]);
+        System.out.println(maze[N][M]);
     }
-    
-    public static void BFS(){
+    static void BFS(){
         Queue<Integer> qx=new LinkedList<>();
-        Queue<Integer> qy=new LinkedList<>();
-        int[] dx={0,1,0,-1};
-        int[] dy={1,0,-1,0};
+        Queue<Integer> qy=new LinkedList<>();     
+        int[] dx={-1,0,1,0};
+        int[] dy={0,-1,0,1};
         
-        qx.add(0);
-        qy.add(0);
-        checked[0][0]=true;
-        
+        qx.add(1);
+        qy.add(1);
         while(!qx.isEmpty()){
             int x=qx.poll();
             int y=qy.poll();
             for(int i=0;i<4;i++){
                 int xx=x+dx[i];
                 int yy=y+dy[i];
-                if(xx>=0&&yy>=0&&xx<N&&yy<M){
+                if(xx>=0&&yy>=0&&xx<=N&&yy<=M){
                     if(maze[xx][yy]==1&&checked[xx][yy]==false){
                         qx.add(xx);
                         qy.add(yy);
                         checked[xx][yy]=true;
-                        maze[xx][yy]=maze[x][y]+1; 
+                        maze[xx][yy]=maze[x][y]+1;
                     }
                 }
             }
---
+++ Class/src/bj2667.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Collections;
+public class bj2667{
+    static int N;
+    static int board[][];
+    static boolean checked[][];
+    static int count;
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        N=Integer.parseInt(br.readLine()); //지도의 크기
+        
+        board=new int[N][N];
+        checked=new boolean[N][N];
+        for(int i=0;i<N;i++){
+            StringTokenizer st=new StringTokenizer(br.readLine());
+            String str=st.nextToken();
+            for(int j=0;j<N;j++){
+                board[i][j]=str.charAt(j)-'0';
+            }
+        }
+        
+        ArrayList<Integer> result=new ArrayList<>();
+        
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                count=0;
+                if(board[i][j]==1&&checked[i][j]==false){
+                    DFS(i,j);
+                    result.add(count);
+                }
+            }
+        }
+        
+        Collections.sort(result);
+        System.out.println(result.size());
+        for(int i=0;i<result.size();i++){
+            System.out.println(result.get(i));
+        }
+    }
+    
+    static void DFS(int a, int b){
+        int[] dx={-1,0,1,0};
+        int[] dy={0,-1,0,1};
+        
+        checked[a][b]=true;
+        count++;
+        
+        for(int i=0;i<4;i++){
+            int x=a+dx[i];
+            int y=b+dy[i];
+            if(x>=0&&y>=0&&x<N&&y<N){
+                if(board[x][y]==1&&checked[x][y]==false){
+                    DFS(x,y);
+                }
+            }
+        }
+    }
+}
